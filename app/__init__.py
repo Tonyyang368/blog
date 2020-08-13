4b7cecd415764d1f3ee4b7ceccac38e75e52b17f
Tony <jyang13@ualberta.ca>
2020-08-12 18:37:59 -0600

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging 
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+from flask_mail import Mail
+import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -32,15 +36,15 @@ if not app.debug:
         app.logger.addHandler(mail_handler)
 
         if not os.path.exists('logs'):
-            os.mkdir('dir')
-        file_handler = RotatingFileHandler('logs/blog.log', maxBytes = 10240,
-        backupCount = 10)
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/blog.log', maxBytes = 10240, backupCount = 10)
         file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s %(message)s [in %(pathname)s:%(lineno)d]'))
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Blog startup')
 
-
+mail = Mail(app)
+bootstrap = Bootstrap(app)
--- config.py
+++ config.py
@@ -11,4 +11,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None 
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['jyang13@ualberta.ca']
+    ADMINS = ['tonyyang368@gmail.com']
+    SECURITY_EMAIL_SENDER = 'tonyyang368@gmail.com'
+    POSTS_PER_PAGE = 5
