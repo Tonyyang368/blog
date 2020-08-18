571bcd9c43fa846944cf429805ec1053404ff68d
Tony <jyang13@ualberta.ca>
2020-08-18 11:41:06 -0600

    test

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -41,8 +41,8 @@ def register():
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
+#        db.session.add(user)
+ #       db.session.commit()
         flash(('Congratulations, you are now a registered user!'))
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', title=('Register'),
