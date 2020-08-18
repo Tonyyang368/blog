de638962333bfc2618279a728c81594f519b840a
Tony <jyang13@ualberta.ca>
2020-08-18 11:30:22 -0600

    'changes'

--- app/models.py
+++ app/models.py
@@ -1,15 +1,12 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db, login
+from flask import current_app
 from flask_login import UserMixin
 from hashlib import md5
 from time import time 
 import jwt 
-from app import app
+from app import db, login
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 followers = db.Table('followers', 
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -69,7 +66,8 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password':self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            app.config['SECRET_KEY'], 
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod 
     def verify_reset_password_token(token):
@@ -80,9 +78,13 @@ class User(UserMixin, db.Model):
             return 
         return User.query.get(id)
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(200), index=True, unique=True)
+    body = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
